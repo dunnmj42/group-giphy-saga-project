3ce59dde44be196337cf3fa4076255d4c597fa6e
Connor Detviler <connor.detviler@gmail.com>
2021-01-28 14:11:30 -0600

    began work on conditionally rendering button and selector

--- src/components/GifItem/GifItem.jsx
+++ src/components/GifItem/GifItem.jsx
@@ -5,15 +5,22 @@ import './GifItem.css'
 function GifItem({gif}) {
     console.log(gif)
 
+    const currentPage = 'FavoritesView';
+    // const currentPage = 'SearchView'
+
     return (
 
         <div className="gif-item">
             <img className="gif" src={gif.url} alt=""/>
             <button>Favorite</button>
+            {/* TODO: make select a controlled input */}
             <select name="" id="">
-                <option value="">Category 1</option>
-                <option value="">Category 2</option>
-                <option value="">Category 3</option>
+                <option value={null} >Select a category</option>
+                <option value="1">Funny</option>
+                <option value="2">Cohort</option>
+                <option value="3">Cartoon</option>
+                <option value="4">NSFW</option>
+                <option value="5">Meme</option>
             </select>
         </div>
 
