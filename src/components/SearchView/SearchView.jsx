0047b03a6eeffbe27064202be51f9559132e7550
Connor Detviler <connor.detviler@gmail.com>
2021-01-28 14:46:45 -0600

    search function displays gifItems on DOM - favorite button adds gif to to favoritereducer

--- src/components/SearchView/SearchView.jsx
+++ src/components/SearchView/SearchView.jsx
@@ -1,10 +1,13 @@
 import SearchForm from '../SearchForm/SearchForm'
+import GifList from '../GifList/GifList'
 
 const SearchView = () => {
 
     return (
-        <SearchForm />
-
+        <div>
+            <SearchForm />
+            <GifList />
+        </div>
     )
 }
 
