a41f3d1344b2c91d19a5d87f6a6b373a707896c7
Michael Dunn <dunnmj42@gmail.com>
2021-01-28 10:50:16 -0600

    index format

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import logger from "redux-logger";
-import App from './App';
+import App from "./App";
 import createSagaMiddleware from "redux-saga";
 import { takeEvery, put } from "redux-saga/effects";
 import axios from "axios";
 
 const resultsList = (state = [], action) => {
   switch (action.type) {
-    case 'SET_RESULTS':
-      return action.payload
+    case "SET_RESULTS":
+      return action.payload;
     default:
       return state;
   }
@@ -19,8 +19,8 @@ const resultsList = (state = [], action) => {
 
 const favoritesList = (state = [], action) => {
   switch (action.type) {
-    case 'SET_FAVORITES':
-      return action.payload
+    case "SET_FAVORITES":
+      return action.payload;
     default:
       return state;
   }
@@ -30,18 +30,18 @@ function* rootSaga() {
   yield takeEvery("SEARCH", setResults);
   yield takeEvery("FETCH_RESULTS", fetchResults);
   yield takeEvery("NEW_FAVORITE", addFavorite);
-};
+}
 
 function* setResults(action) {
   try {
-    const q = action.payload
+    const q = action.payload;
     const response = yield axios.get(`/api/search/${q}`);
     console.log(response.data);
     yield put({ type: "SET_RESULTS", payload: response.data });
   } catch (error) {
     console.error(error);
   }
-};
+}
 
 function* fetchResults() {
   try {
@@ -51,29 +51,33 @@ function* fetchResults() {
   } catch (error) {
     console.error(error);
   }
-};
+}
 
 function* addFavorite(action) {
   try {
     const newFavorite = action.payload;
     yield axios.post("/api/favorite", newFavorite);
-    yield put({type: 'FETCH_RESULTS'})
+    yield put({ type: "FETCH_RESULTS" });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-};
+}
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   combineReducers({
     resultsList,
-    favoritesList
+    favoritesList,
   }),
   applyMiddleware(sagaMiddleware, logger)
 );
 
 sagaMiddleware.run(rootSaga);
 
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
