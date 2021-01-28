a0d01ea0130842b24eaaafa61ee5368143313b17
Connor Detviler <connor.detviler@gmail.com>
2021-01-28 13:48:57 -0600

    added css grid to organize gifs, added buttons and a selector to each gifitem

--- src/components/GifItem/GifItem.jsx
+++ src/components/GifItem/GifItem.jsx
@@ -2,13 +2,20 @@ import {useSelector, useDispatch} from 'react-redux';
 import './GifItem.css'
 
 
-function GifItem() {
+function GifItem({gif}) {
+    console.log(gif)
 
     return (
 
-        <p>
-            s;lkethlakjrhg
-        </p>
+        <div className="gif-item">
+            <img className="gif" src={gif.url} alt=""/>
+            <button>Favorite</button>
+            <select name="" id="">
+                <option value="">Category 1</option>
+                <option value="">Category 2</option>
+                <option value="">Category 3</option>
+            </select>
+        </div>
 
     )
 
--- src/components/GifList/GifList.jsx
+++ src/components/GifList/GifList.jsx
@@ -2,18 +2,37 @@ import {useSelector, useDispatch} from 'react-redux';
 import GifItem from '../GifItem/GifItem.jsx';
 import './GifList.css'
 
-const dummyGifs = [{title: "boo", url:"https://media0.giphy.com/media/JO4Z10C3YDwjaj5xP3/giphy.gif"},
-    {title: "features", url: "https://media0.giphy.com/media/U4YVpervbNOSRA7ysy/giphy.gif"},
-    {title: "cheese", url: "https://media0.giphy.com/media/3ov9k4RPBJPEGCHwtO/giphy.gif"}
+const dummyGifs = [
+    {
+        id: 1,
+        url:"https://media0.giphy.com/media/JO4Z10C3YDwjaj5xP3/giphy.gif",
+        title: "boo",
+        cat_id: 1
+    },
+    {
+        id: 2,
+        url: "https://media0.giphy.com/media/U4YVpervbNOSRA7ysy/giphy.gif",
+        title: "features",
+        cat_id: 4
+    },
+    {
+        id: 3,
+        url: "https://media0.giphy.com/media/3ov9k4RPBJPEGCHwtO/giphy.gif",
+        title: "cheese",
+        cat_id: 2
+    }
 ]
 
 function GifList() {
 
 
     return (
-        <div>
+        <div className="container">
             {dummyGifs.map((gif) => {
-                return (<GifItem />)
+                return (<GifItem
+                            key={gif.id}
+                            gif={gif}
+                        />)
             })}
         </ div>
     )
