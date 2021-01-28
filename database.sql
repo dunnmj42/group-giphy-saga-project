10959f0b7503b7fd07ce004a021d451f78ae09e6
Michael Dunn <dunnmj42@gmail.com>
2021-01-28 09:37:40 -0600

    SQL order

--- database.sql
+++ database.sql
@@ -2,17 +2,18 @@ CREATE DATABASE "giphy_search_favorites";
 
 -- You'll need a table for storing each giphy image favorite
 -- Each favorite image can be assigned 1 of the following categories as a Foreign Key
-CREATE TABLE "favorites" (
-    "id" SERIAL PRIMARY KEY,
-    "url" VARCHAR (100) NOT NULL,
-    "title" VARCHAR (255) NOT NULL,
-    "cat_id" INT REFERENCES "category"
-);
 
 -- Category table
 CREATE TABLE "category" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR (100) NOT NULL
+);
+
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "url" VARCHAR (100) NOT NULL,
+    "title" VARCHAR (255) NOT NULL,
+    "cat_id" INT REFERENCES "category"
 );
 
 -- Default categories. You may change them :)
