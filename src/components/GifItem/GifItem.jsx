c43c317e2731c676c7b004f64ce851cc2fc963a2
Michael Dunn <dunnmj42@gmail.com>
2021-01-29 10:39:56 -0600

    code format consistency

--- src/components/GifItem/GifItem.jsx
+++ src/components/GifItem/GifItem.jsx
@@ -17,31 +17,30 @@ function GifItem({ gif, favorites }) {
   };
 
   const removeFavorite = () => {
-
     dispatch({
-        type: 'REMOVE_FAVORITE',
-        payload: gif
-    })
-  }
+      type: "REMOVE_FAVORITE",
+      payload: gif,
+    });
+  };
 
   return (
     <div className="gif-item">
       <img className="gif" src={gif.url} alt={gif.title} />
       <div>
         {favorites ? (
-            <>
+          <>
             <select name="" id="" onChange={selectCat}>
-            <option value={null}>Select a category</option>
-            <option value="1">Funny</option>
-            <option value="2">Cohort</option>
-            <option value="3">Cartoon</option>
-            <option value="4">NSFW</option>
-            <option value="5">Meme</option>
+              <option value={null}>Select a category</option>
+              <option value="1">Funny</option>
+              <option value="2">Cohort</option>
+              <option value="3">Cartoon</option>
+              <option value="4">NSFW</option>
+              <option value="5">Meme</option>
             </select>
             <button onClick={removeFavorite}>Remove Favorite</button>
-            </>
+          </>
         ) : (
-            <button onClick={addFavorite}>Favorite</button>
+          <button onClick={addFavorite}>Favorite</button>
         )}
       </div>
     </div>
